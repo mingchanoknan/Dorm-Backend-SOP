77c7d2989ad8fdbd735ef924b129d0c4377a5f17
mingchanoknan <63070027@kmitl.ac.th>
2022-12-21 19:50:21 +0700

    create room grpc

---
+++ room-service-gradle/src/main/java/com/dorm/roomservicegradle/RoomServiceGradleApplication.java
@@ -0,0 +1,13 @@
+package com.dorm.roomservicegradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomServiceGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomServiceGradleApplication.class, args);
+	}
+
+}
